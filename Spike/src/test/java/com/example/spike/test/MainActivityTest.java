70cdb355b37d97b0651c76a392024f3e56f948de
Diego <dpeletei@thoughtworks.com>
2013-10-10 09:13:42 +0200

    First commit

---
+++ Spike/src/test/java/com/example/spike/test/MainActivityTest.java
@@ -0,0 +1,15 @@
+package com.example.spike.test;
+
+import android.test.ActivityTestCase;
+
+
+
+public class MainActivityTest extends ActivityTestCase {
+
+    public void testTrue(){
+       assertEquals(true,1==0);
+    }
+
+
+
+}
