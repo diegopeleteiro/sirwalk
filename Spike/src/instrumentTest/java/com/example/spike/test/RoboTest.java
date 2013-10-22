c0e19fc8be3b2169a24d22d4eca7b99a3825fcfd
Diego <dpeletei@thoughtworks.com>
2013-10-22 09:27:35 +0200

    Initial commit

---
+++ Spike/src/instrumentTest/java/com/example/spike/test/RoboTest.java
@@ -0,0 +1,33 @@
+package com.example.spike.test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Test.*;
+
+import android.os.*;
+import android.os.Bundle;
+import android.test.ActivityTestCase;
+import android.view.View;
+
+import com.example.spike.MainActivity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class RoboTest{
+
+    private MainActivity mainActivity;
+
+    @Test
+    public void testIncreaseSensitivityShouldBe31(){
+        mainActivity = new MainActivity();
+
+        mainActivity.onCreate(new Bundle());
+
+        mainActivity.getIncreaseButton().performClick();
+
+        assertEquals(31, mainActivity.getSensitivity());
+
+    }
+}
